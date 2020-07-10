65ba1bf6aa6e7edcb76209494ad17234a2bf3755
murad-narimanli <murad.narimanli@mail.ru>
2020-07-10 12:46:12 +0400

    secon

---
+++ build/precache-manifest.e0b03438605702ff4bacb7fae2d7a417.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dacc295baf27fc9b36f670240d551298",
+    "url": "/index.html"
+  },
+  {
+    "revision": "79196af6c427403306cb",
+    "url": "/static/css/main.aa01ec09.chunk.css"
+  },
+  {
+    "revision": "d0bc57468db9503c3a27",
+    "url": "/static/js/2.092e244c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.092e244c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79196af6c427403306cb",
+    "url": "/static/js/main.b0764103.chunk.js"
+  },
+  {
+    "revision": "441cfea73f775386a3f5",
+    "url": "/static/js/runtime-main.02030f09.js"
+  }
+]);
